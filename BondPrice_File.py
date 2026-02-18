df4719cc8e47ba8db08e509e795075ba21896a42
zz3372 <zz3372@columbia.edu>
2026-02-18 16:12:41 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,16 +1,18 @@
 
-
 def getBondPrice(y, face, couponRate, m, ppy=1):
-   pvcsum = 0
 
-    for i in range(1, m * ppy + 1):
+    pvcsum = 0
+    n = int(m * ppy)
+
+    for i in range(1, n + 1):
 
         cf = face * couponRate / ppy
         pvm = (1 + y / ppy) ** (-i)
         pvcf = pvm * cf
         pvcsum = pvcf + pvcsum
 
-        if i = m * ppy:
+        if i == n:
             pvcsum = pvcsum + face * pvm
+         
 
     return(pvcsum)
