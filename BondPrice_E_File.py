a409ac604fffa934ea275d1d71ca1a76012cadeb
zz3372 <zz3372@columbia.edu>
2026-02-20 18:44:04 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,12 @@
-
-
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    
+    c = face * couponRate
+    price = 0.0
+    n = len(yc)
+    
+    for t, y in enumerate(yc, start=1):
+        cf = c if t < n else (c + face)
+        pv = 1 / (1 + y) ** t
+        price += cf * pv
+
+    return price
